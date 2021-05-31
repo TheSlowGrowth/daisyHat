688ac41e6941c377b16930d3f66e9958b67415e9
TheSlowGrowth <9356320+TheSlowGrowth@users.noreply.github.com>
2021-05-31 23:48:59 +0200

    Added Dockerfile and docker orchestrator script

---
+++ docker/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+OWNER=$1
+REPO=$2
+PAT=$3
+NAME=$4
+# if set this script will only run ./config.sh
+# it will not run the actions runner
+REGISTER_ONLY=$5
+
+cleanup() {
+    token=$(curl -s -XPOST -H "authorization: token ${PAT}" \
+        https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token |\
+        jq -r .token)
+    ./config.sh remove --token $token
+}
+
+token=$(curl -s -XPOST \
+    -H "authorization: token ${PAT}" \
+    https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token |\
+    jq -r .token)
+
+./config.sh \
+    --url https://github.com/${OWNER}/${REPO} \
+    --token ${token} \
+    --name ${NAME} \
+    --work _work \
+    --labels daisyHat \
+    --unattended \
+    --replace
+
+if [ -z ${REGISTER_ONLY} ]; then
+    ./run.sh --once
+    cleanup
+fi
