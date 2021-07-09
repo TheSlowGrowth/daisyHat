38f00435617c043c4958a03f0e20f3657d3c0d05
TheSlowGrowth <9356320+TheSlowGrowth@users.noreply.github.com>
2021-07-09 22:23:40 +0200

    feat(libDaisyHat): add multiple integer types to string conversion functions

--- src/stringConversions.h
+++ src/stringConversions.h
@@ -3,6 +3,15 @@
 
 const char* ToString(bool value);
 daisy::FixedCapStr<128> ToString(float value);
+daisy::FixedCapStr<128> ToString(unsigned int value);
 daisy::FixedCapStr<128> ToString(int value);
+daisy::FixedCapStr<128> ToString(uint8_t value);
+daisy::FixedCapStr<128> ToString(int8_t value);
+daisy::FixedCapStr<128> ToString(uint16_t value);
+daisy::FixedCapStr<128> ToString(int16_t value);
+daisy::FixedCapStr<128> ToString(uint32_t value);
+daisy::FixedCapStr<128> ToString(int32_t value);
+daisy::FixedCapStr<128> ToString(uint64_t value);
+daisy::FixedCapStr<128> ToString(int64_t value);
 const char ToString(char value);
 const char* ToString(const char* value);
